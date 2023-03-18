f697720bf7fce02a7d7acb9beffdc5bea3a140b6
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-18 17:43:16 -0300

    setup mf-shell host project

---
+++ projects/mf-shell/webpack.config.js
@@ -0,0 +1,14 @@
+const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+
+module.exports = withModuleFederationPlugin({
+
+  remotes: {
+    "mfPayment": "http://localhost:4200/remoteEntry.js",
+    "mfShopping": "http://localhost:4200/remoteEntry.js",    
+  },
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+});
