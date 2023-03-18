1e7e81091a5947ef14e89341b236fc3489ccf824
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-18 17:55:13 -0300

    setup mf-payment remote

--- projects/mf-payment/webpack.config.js
+++ projects/mf-payment/webpack.config.js
@@ -2,9 +2,10 @@ const { shareAll, withModuleFederationPlugin } = require('@angular-architects/mo
 
 module.exports = withModuleFederationPlugin({
 
-  remotes: {
-    "mfPayment": "http://localhost:4202/remoteEntry.js",
-    "mfShopping": "http://localhost:4201/remoteEntry.js",    
+  name: 'mf-payment',
+
+  exposes: {
+    './Component': './projects/mf-payment/src/app/app.component.ts',
   },
 
   shared: {
