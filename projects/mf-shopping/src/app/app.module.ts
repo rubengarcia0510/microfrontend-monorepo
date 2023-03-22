ffa6c0c9895c0d8b334b0ac210a3be437752f01b
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-22 11:47:00 -0300

    delete products component in mf-shopping

--- projects/mf-shopping/src/app/app.module.ts
+++ projects/mf-shopping/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
