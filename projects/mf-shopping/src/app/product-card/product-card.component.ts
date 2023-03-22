ed04a80ee06277ce20ef93479e7ca1db9a1efcc0
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-22 18:05:36 -0300

    clickCard() method

--- projects/mf-shopping/src/app/product-card/product-card.component.ts
+++ projects/mf-shopping/src/app/product-card/product-card.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonsLibService } from '@commons-lib';
+import { IProductCard } from '../models/product-card.interface';
 
 @Component({
   selector: 'app-product-card',
@@ -8,5 +10,15 @@ import { Component } from '@angular/core';
   imports: [CommonModule]
 })
 export class ProductCardComponent {
+  @Input() product?: IProductCard;
+
+  constructor(private _commonsLibService: CommonsLibService) {}
+
+  clickCard(): void {
+    this._commonsLibService.sendData({
+      name: this.product!.name,
+      price: this.product!.price,
+    });
+  }
 
 }
