53c65ad5749645c7f84fadcbb2b467af32e258b7
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-22 13:46:43 -0300

    create product-card component in mf-shopping

---
+++ projects/mf-shopping/src/app/product-card/product-card.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+  imports: [CommonModule]
+})
+export class ProductCardComponent {
+
+}
