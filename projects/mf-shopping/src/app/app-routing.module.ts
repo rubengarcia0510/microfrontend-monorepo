e503e607f344c6540e327daef2d124024a279e33
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-22 12:59:09 -0300

    create products component in mf-shoppinglink product with mf-shopping

--- projects/mf-shopping/src/app/app-routing.module.ts
+++ projects/mf-shopping/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   {
     path:'',
     loadChildren:()=>
-      import('./products/products.component').then((m)=>m.ProductsComponent) 
+      import('./products/products.module').then((m)=>m.ProductsModule) 
   }
 ];
 
