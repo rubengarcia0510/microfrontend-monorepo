8eab0fa745019d4e5275df535ff635023a0b1f42
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-22 21:44:04 -0300

    add constructor and commonService

--- projects/mf-shell/src/app/app.component.ts
+++ projects/mf-shell/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonsLibService } from '@commons-lib';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'mf-shell';
+
+  constructor(public commonsLibService: CommonsLibService) {}
 }
