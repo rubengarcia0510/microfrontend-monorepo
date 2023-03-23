6841b82a3c2d607be65efb88c2fddbba145b3103
Ruben Garcia <rubengarcia0510@gmail.com>
2023-03-23 09:34:29 -0300

    imports: [CommonModule]

--- projects/mf-shopping/src/app/product-card/product-card.component.ts
+++ projects/mf-shopping/src/app/product-card/product-card.component.ts
@@ -4,9 +4,11 @@ import { CommonsLibService } from '@commons-lib';
 import { IProductCard } from '../models/product-card.interface';
 
 @Component({
+  standalone: true,
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
-  styleUrls: ['./product-card.component.scss']
+  styleUrls: ['./product-card.component.scss'],
+  imports: [CommonModule]
 })
 export class ProductCardComponent {
   @Input() product?: IProductCard;
